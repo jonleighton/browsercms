f7cfd81763b9a810155ce814340607093bfc97e0
Jon Leighton <j@jonathanleighton.com>
2009-08-18 01:19:56 +0100

    Prevent routes being loaded by a 'host' application. This was already done in 96d01da1a3b4c3a2ad010458c1668fb3d7222096 but accidentally lost when I merged the permissions branch.
    Conflicts:
    
    	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
-if RAILS_ENV == "test"
-  class ContentBlock
-    def self.versioned?; true; end
-    def self.publishable?; true; end
-    def self.connectable?; true; end
-    def self.searchable?; false; end
+# Only load these routes if this is the "root" application
+if RAILS_ROOT == File.expand_path(File.dirname(__FILE__) + "/..")
+  if RAILS_ENV == "test"
+  	class ContentBlock
+      def self.versioned?; true; end
+      def self.publishable?; true; end
+      def self.connectable?; true; end
+      def self.searchable?; false; end
+    end
   end
-end      
 
-ActionController::Routing::Routes.draw do |map|
-  map.connect "/__test__", :controller => "cms/content", :action => "show_page_route"
-  map.routes_for_browser_cms
+  ActionController::Routing::Routes.draw do |map|
+    map.connect "/__test__", :controller => "cms/content", :action => "show_page_route"
+    map.routes_for_browser_cms
+  end
 end
